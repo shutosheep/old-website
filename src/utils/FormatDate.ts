2130099e1603a74db9ac56417631c04263bee14c
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-01-01 14:39:18 +0900

    update: add date format options, update display format

--- src/utils/FormatDate.ts
+++ src/utils/FormatDate.ts
@@ -1,3 +1,9 @@
+const formatOptions : object = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+}
+
 export function formatDate(date: string): string {
-    return new Date(date).toLocaleDateString("en-us");
+    return new Date(date).toLocaleDateString("en-us", formatOptions);
 }
