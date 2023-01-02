c206a3dba4558ea806fcde660d51beedb6eeb932
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-01-02 23:14:40 +0900

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://shutotamaoka.xyz'
+    site: 'https://shutotamaoka.xyz',
+    integrations: [sitemap()],
 });
