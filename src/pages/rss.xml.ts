44c4e82db9f914be8be8b7e73a3820a03a44101b
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-01-02 13:59:52 +0900

    feat: add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,9 @@
+import rss from '@astrojs/rss';
+
+export const get = () => rss({
+    title: "Shuto's website",
+    description: "Shuto Tamaoka's personal website",
+    site: import.meta.env.SITE,
+    items: import.meta.glob('./blog/*.md'),
+    customData: `<language>en-us</language>`,
+})
