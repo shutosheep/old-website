e2b1ce40c97c09a4744504f4c127247ce7943483
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-01-27 13:52:13 +0900

    refactor: use content collections for listing items

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SortBlogPosts } from '@utils/SortBlogPosts';
+
+const site = new URL(import.meta.env.SITE).href;
+const blogPosts = SortBlogPosts(await getCollection('blog'));
 
 export const get = () => rss({
     title: "Shuto's website",
     description: "Shuto Tamaoka's personal website",
-    site: import.meta.env.SITE,
-    items: import.meta.glob('./blog/*.md'),
+    site: site,
+    items: blogPosts.map((post) => ({
+        link: `${site}blog/${post.slug}`,
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+    })),
     customData: `<language>en-us</language>`,
     stylesheet: '/rss/pretty-feed-v3.xsl',
 })
