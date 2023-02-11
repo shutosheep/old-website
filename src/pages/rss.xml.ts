19ad7199f55c9dde90ea2ef0d201c44f62a7056a
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-02-11 23:03:11 +0900

    feat: add full post content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,10 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SortBlogPosts } from '@utils/SortBlogPosts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
+const parser = new MarkdownIt();
 const site = new URL(import.meta.env.SITE).href;
 const blogPosts = SortBlogPosts(await getCollection('blog'));
 
@@ -14,6 +17,7 @@ export const get = () => rss({
         title: post.data.title,
         pubDate: post.data.pubDate,
         description: post.data.description,
+        content: sanitizeHtml(parser.render(post.body)),
     })),
     customData: `<language>en-us</language>`,
     stylesheet: '/rss/pretty-feed-v3.xsl',
